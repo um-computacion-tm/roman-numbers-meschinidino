771b99e2de7654c170af1f50bef6cd78a9b380b8
meschinidino <f.meschini@alumno.um.edu.ar>
2023-03-21 20:57:50 -0300

    resuelve hasta 3 digitos

--- roman_numbers.py
+++ roman_numbers.py
@@ -1,8 +1,25 @@
 #Programa que convierte decimal a romano
 import unittest
 def decimal_to_roman(decimal):
+    return generar_numero(decimal)
+def generar_numero(decimal):
+    numero = str(decimal)
+    if len(numero) == 3:
+        centena = tres_digitos(decimal)
+        decimal = int(numero[1:])
+        decena = dos_digitos(decimal)
+        decimal = int(numero[-1])
+        unidad = un_digito(decimal)
+        return centena + decena + unidad
+    if len(numero) == 2:
+        decena = dos_digitos(decimal)
+        decimal = int(numero[-1])
+        unidad = un_digito(decimal)
+        return decena + unidad
+    if len(numero) == 1:
+        return un_digito(decimal)
 
-def un_digito(numero):
+def un_digito(decimal):
     if decimal <= 3:
         return "I" * decimal
     if decimal == 4:
@@ -13,10 +30,30 @@ def un_digito(numero):
         if decimal <= 8:
             return "V" + ((decimal - 5) * "I")
 def dos_digitos(numero):
-    return letra
+    decimal = int(str(numero)[0])
+    if decimal <= 3:
+        return "X" * decimal
+    if decimal == 4:
+        return "XL"
+    else:
+        if decimal > 8:
+            return ((10 - decimal) * "X") + "C"
+        if decimal <= 8:
+            return "L" + ((decimal - 5) * "X")
+
 def tres_digitos(numero):
+    decimal = int(str(numero)[0])
+    if decimal <= 3:
+        return "C" * decimal
+    if decimal == 4:
+        return "CD"
+    else:
+        if decimal > 8:
+            return ((10 - decimal) * "C") + "D"
+        if decimal <= 8:
+            return "D" + ((decimal - 5) * "C")
     return letra
-def cuaatro_digitos(numero):
+def cuatro_digitos(numero):
     return letra
 
 class TestDecimalToRoman(unittest.TestCase):
@@ -53,5 +90,8 @@ class TestDecimalToRoman(unittest.TestCase):
     def test_cien(self):
         resultado = decimal_to_roman(100)
         self.assertEqual(resultado, "C")
+    def test_quinientoscincuentaycinco(self):
+        resultado = decimal_to_roman(555)
+        self.assertEqual(resultado, "DLV")
 if __name__ == "__main__":
     unittest.main()
